bce789c8c02b3b85c84d2829281b6a21002e2092
rcs-nieruchomosci <82536785+rcs-nieruchomosci@users.noreply.github.com>
2023-10-22 17:01:35 +0000

    work in progess

--- init.js
+++ init.js
@@ -1,19 +1,25 @@
 // init.js
+require('dotenv').config();
 const puppeteer = require('puppeteer');
 
-let browser;
-let page;
-
 async function initialize() {
-    browser = await puppeteer.launch({ headless: false, args: ['--no-sandbox'] });
-    page = await browser.newPage();
-// Login
-console.log("Navigating to app.nbot.pl");
-await page.goto('https://app.nbot.pl/');
-console.log("Typing credentials...");
-await page.type('input[name="login"]', process.env.USERNAME);
-await page.type('input[name="password"]', process.env.PASSWORD);
-    return { browser, page };
-}
+  const browser = await puppeteer.launch({ headless: false, args: ['--no-sandbox'] });
+  const page = await browser.newPage();
 
+  // Login
+  console.log("Navigating to app.nbot.pl");
+  await page.goto('https://app.nbot.pl/');
+    console.log("Typing credentials...");
+  await page.type('input[name="login"]', process.env.USERNAME);
+  await page.type('input[name="password"]', process.env.PASSWORD);
+    console.log("Credentials typed!");
+    const buttons = await page.$x('//button[text()="Zaloguj się"]');
+    if (buttons.length > 0) {
+        await buttons[0].click();
+    } else {
+        console.log('Button not found');
+    }
+    console.log("Logged in!");
+  return { browser, page };
+}
 module.exports = initialize;
--- scraper.js
+++ scraper.js
@@ -1,87 +1,118 @@
-const puppeteer = require('puppeteer');
-const fs = require('fs');
-const initialize = require('./init.js');
+const { takeScreenshot } = require('./screenshotUtils');
+
+const MAX_PAGES = 25;
 
 async function extractAndSavePhoneNumber(page) {
-    const phoneNumber = await page.$eval('button.btn.btn-list.btn-block.ng-star-inserted span', span => span.textContent.trim());
-    console.log(phoneNumber);
+    // Assuming the phone number is within a specific span. Modify the selector as necessary.
+    const phoneNumberSpan = await page.$('button.btn.btn-list.btn-block.ng-star-inserted span');
+    
+    if (phoneNumberSpan) {
+        const phoneNumber = await page.evaluate(span => span.textContent, phoneNumberSpan);
+        // Save or process the phone number as needed. For now, just logging it:
+        console.log(`Extracted phone number: ${phoneNumber}`);
+        return phoneNumber;
+    }
+    throw new Error('Phone number not found.');
+    }
 
-    // Save to the in-memory array
-    fs.appendFileSync('phone_numbers.txt', phoneNumber + '\n', 'utf-8');
+async function navigateToOffersPage(page) {
+    console.log("Clicking offers...");
+    await page.waitForXPath('//a[@href="#/base/offers"]', { visible: true });
+    await takeScreenshot(page, 'before_clicking_offers.png');
+    
+    const offersLink = await page.$x('//a[@href="#/base/offers"]');
+    
+    if (offersLink.length === 0) {
+        throw new Error('Offers link not found on the page.');
+    }
+    
+    await page.waitForTimeout(1000); // Added delay for safety
+
+    try {
+        await offersLink[0].click();
+    } catch {
+        console.log("Normal click failed. Trying with coordinates.");
+        const rect = await offersLink[0].boundingBox();
+        await page.mouse.click(rect.x + rect.width / 2, rect.y + rect.height / 2);
+    }
+
+    await page.waitForTimeout(5000); // Wait for offers page to load
 }
 
-async function scrapeData() {
-    const { browser, page } = await initialize();
-    const phoneNumbers = [];
-
-    page.setViewport({
-        width: 768,
-        height: 1024
-    });
-    page.setDefaultNavigationTimeout(60000);
-    page.setDefaultTimeout(60000);
-
-    // Navigate to the offers section once
-    console.log("Clicking offers...");
-    await page.waitForXPath('//a[@href="#/base/offers"]');
-    const offersLink = await page.$x('//a[@href="#/base/offers"]');
-    await offersLink[0].click();
-
-    // Wait for offers page to load
-    await page.waitForTimeout(5000);
-
-    for (let pageIndex = 0; pageIndex < 25; pageIndex++) { // Iterate for 25 pages
-        let hasHiddenNumbers = true;
-
-        while (hasHiddenNumbers) {
-            const hiddenPhoneButtonXpath = '//button[contains(@class, "btn-list") and contains(@class, "btn-block") and contains(@class, "ng-star-inserted")]//span[contains(text(), " *** ")]';
-            const hiddenPhoneButtons = await page.$x(hiddenPhoneButtonXpath);
-            const hiddenPhoneButton = hiddenPhoneButtons[0];
-
-            if (hiddenPhoneButton) {
-                console.log("Clicking phone button...");
-                await hiddenPhoneButton.click();
-
-                try {
-                    await page.waitForFunction(() => {
-                        const phoneNumberSpan = document.querySelector('button.btn.btn-list.btn-block.ng-star-inserted span');
-                        return phoneNumberSpan && !phoneNumberSpan.textContent.includes('***');
-                    }, { timeout: 10000 }); // Wait for 10 seconds max
-
-                    // Extract and immediately save the phone number
-                    await extractAndSavePhoneNumber(page);
-
-                    // Delay between getting phone numbers to avoid getting blocked
-                    await page.waitForTimeout(5000);
-                } catch (err) {
-                    console.warn(`Error while getting number: ${err.message}`);
-                    continue; // Continue with the next button
-                }
-            } else {
-                hasHiddenNumbers = false;
-            }
+async function handlePhoneNumbers(page, shouldContinueScraping) {
+    while (shouldContinueScraping) {
+        const hiddenPhoneButtonXpath = '//button[contains(@class, "btn-list") and contains(@class, "btn-block") and contains(@class, "ng-star-inserted")]//span[contains(text(), " *** ")]';
+        const hiddenPhoneButtons = await page.$x(hiddenPhoneButtonXpath);
+    
+        if (!hiddenPhoneButtons.length) {
+            console.log("No more hidden phone numbers found.");
+            break;
         }
+    
+        const hiddenPhoneButton = hiddenPhoneButtons[0];
+        console.log("Clicking phone button...");
+        await hiddenPhoneButton.click();
+    
+        try {
+            await page.waitForFunction(() => {
+                const phoneNumberSpan = document.querySelector('button.btn.btn-list.btn-block.ng-star-inserted span');
+                return phoneNumberSpan && !phoneNumberSpan.textContent.includes('***');
+            }, { timeout: 10000 });
+    
+            await extractAndSavePhoneNumber(page);
+            await page.waitForTimeout(5000);
+        } catch (err) {
+            console.warn(`Error while getting number: ${err.message}`);
+        }
+    }
+    return shouldContinueScraping; // Return the flag's value so that we can use it in the scrapeData function
+}
 
-        // Navigate to the next page
-        console.log("Navigating to the next page...");
-        const nextPageButton = await page.$('.pagination-next.page-item:not(.disabled) a.page-link');
-        if (nextPageButton) {
-            await Promise.all([
-                nextPageButton.click(),
-                page.waitForNavigation({ waitUntil: 'networkidle0' }),
-            ]);
+async function scrapeData(browser, page, shouldContinueScraping) {
+    console.log(shouldContinueScraping);
+    page.setViewport({
+        width: 1920,
+        height: 1080,
+    });
+    page.setDefaultNavigationTimeout(20000);
+    page.setDefaultTimeout(20000);
+
+    try {
+        await navigateToOffersPage(page);
+    } catch (err) {
+        console.log('Taking a screenshot due to error...');
+        await takeScreenshot(page, 'error_screenshot.png');
+        throw err;
+    }
+
+    for (let pageIndex = 0; pageIndex < MAX_PAGES; pageIndex++) {
+        // Store the return value from handlePhoneNumbers
+        const continueScraping = await handlePhoneNumbers(page, shouldContinueScraping);
+    
+        if (continueScraping) {
+            console.log("Navigating to the next page...");
+            const nextPageButton = await page.$('.pagination-next.page-item:not(.disabled) a.page-link');
+    
+            if (nextPageButton) {
+                await Promise.all([
+                    nextPageButton.click(),
+                    page.waitForNavigation({ waitUntil: 'networkidle0' }),
+                ]);
+            } else {
+                console.log("No more pages to navigate to.");
+                break;
+            }
         } else {
-            console.log("No more pages to navigate to.");
+            console.log("Stopping scraper...");
             break;
         }
     }
-
-    await browser.close();
-    console.log("Done!");
+    
+    // Moved out of the for loop to ensure browser only closes after all processing is done.
+    if (pageIndex === MAX_PAGES - 1 || !continueScraping) {
+        await browser.close();
+        console.log("Done!");
+    }
 }
 
-// Execute the scraping function
-scrapeData().catch(error => {
-    console.error(error);
-    process.exit(1);
-});
+module.exports = scrapeData;
